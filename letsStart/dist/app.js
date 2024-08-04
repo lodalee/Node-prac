ece97188c743037f05d3d593eb8f397899487292
chaewon <cwlee4633@naver.com>
2024-08-04 18:29:51 +0900

    미들웨어

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -3,10 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app_model_1 = require("./app.model");
 var app = express();
-var data = [1, 2, 3, 4];
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is logging middleware");
+    next();
+});
 app.get("/", function (req, res) {
     console.log(req);
     res.send({ cats: app_model_1.Cat });
+});
+app.get("/cats/a", function (req, res) {
+    res.send({ a: app_model_1.Cat[0] });
+});
+app.get("/cats/d", function (req, res) {
+    res.send({ d: app_model_1.Cat[3] });
+});
+app.use(function (req, res, next) {
+    console.log("this is error middleware");
+    res.send({ error: "404 not found error" });
 });
 app.listen(8000, function () {
     console.log("server is on..");
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,13 +1,31 @@
 import * as express from "express";
 import { Cat, CatType } from "./app.model";
+import { error } from "console";
 
 const app: express.Express = express();
 
-const data = [1, 2, 3, 4];
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is logging middleware");
+  next();
+});
 
 app.get("/", (req: express.Request, res: express.Response) => {
   console.log(req);
   res.send({ cats: Cat });
+});
+
+app.get("/cats/a", (req, res) => {
+  res.send({ a: Cat[0] });
+});
+
+app.get("/cats/d", (req, res) => {
+  res.send({ d: Cat[3] });
+});
+
+app.use((req, res, next) => {
+  console.log("this is error middleware");
+  res.send({ error: "404 not found error" });
 });
 
 app.listen(8000, () => {

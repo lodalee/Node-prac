aae2abe5064981ede76f22dc7ad64dc2096f231d
lodalee <cwlee4633@naver.com>
2024-08-09 16:18:43 +0900

    .

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,14 +1,14 @@
 //** Create Read */
 
-import * as express from "express";
-import catsRouter from './cats/cats.route'
+import * as express from 'express';
+import catsRouter from './cats/cats.route';
 
 const app: express.Express = express();
 
-//*  logging middleware
+//* logging middleware
 app.use((req, res, next) => {
   console.log(req.rawHeaders[1]);
-  console.log("this is logging middleware");
+  console.log('this is logging middleware');
   next();
 });
 
@@ -16,71 +16,12 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(catsRouter);
 
-//* READ 고양이 전체 데이터 조회
-app.get('/cats', (req, res) =>{
-  try{
-    const cats = Cat
-    // throw new Error("db connect error")
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      }
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-
-//* READ 고양이 특정 데이터 조회
-app.get('/cat/:id', (req, res) =>{
-  try{
-    const params = req.params;
-    const cat = Cat.find((cat)=> {
-      return cat.id === params.id;
-    });
-    // throw new Error("db connect error")
-    res.status(200).send({
-      success: true,
-      data: {
-        cat,
-      }
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-
-//* CREATE 새로운 고양이 추가api
-app.post('/cats', (req, res)=>{
-  try{
-    const data = req.body;
-    Cat.push(data);
-    res.status(200).send({
-      success: true,
-      data: {data}
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-})
-
-
 //* 404 middleware
 app.use((req, res, next) => {
-  console.log("this is error middleware");
-  res.send({ error: "404 not found error" });
+  console.log('this is error middleware');
+  res.send({ error: '404 not found error' });
 });
 
 app.listen(8000, () => {
-  console.log("server is on..");
+  console.log('server is on...');
 });
--- letsStart/src/cats/cats.model.ts
+++ letsStart/src/cats/cats.model.ts
@@ -1,44 +1,51 @@
 export type CatType = {
-    id: string;
-    name: string;
-    age: number;
-    species: string;
-    isCute: boolean;
-    friends: string[];
-  };
-  
-  export const Cat: CatType[] = [
-    {
-      id: "1",
-      name: "a",
-      age: 2,
-      species: "cat1",
-      isCute: true,
-      friends: ["2", "3"],
-    },
-    {
-      id: "2",
-      name: "b",
-      age: 2,
-      species: "cat2",
-      isCute: true,
-      friends: ["1", "3"],
-    },
-    {
-      id: "3",
-      name: "c",
-      age: 4,
-      species: "cat3",
-      isCute: true,
-      friends: ["1", "2", "4"],
-    },
-    {
-      id: "4",
-      name: "d",
-      age: 3,
-      species: "cat3",
-      isCute: true,
-      friends: [],
-    },
-  ];
-  
+  id: string;
+  name: string;
+  age: number;
+  species: string;
+  isCute: boolean;
+  friends: string[];
+};
+
+export const Cat: CatType[] = [
+  {
+    id: 'fsduifh',
+    name: 'blue',
+    age: 8,
+    species: 'Russian Blue',
+    isCute: true,
+    friends: ['asdfhj29009', 'WE09tju2j'],
+  },
+  {
+    id: 'iohf2309q4hr',
+    name: 'som',
+    age: 4,
+    species: 'Sphynx cat',
+    isCute: true,
+    friends: ['weju0fj20qj', 'asdfhj29009', 'weju0fj20qj'],
+  },
+  {
+    id: 'WE09tju2j',
+    name: 'lean',
+    age: 6,
+    species: 'Munchkin',
+    isCute: false,
+    friends: [],
+  },
+  {
+    id: 'asdfhj29009',
+    name: 'star',
+    age: 10,
+    species: 'Scottish Fold',
+    isCute: true,
+    friends: ['weju0fj20qj'],
+  },
+  {
+    id: 'weju0fj20qj',
+    name: 'red',
+    age: 2,
+    species: 'Sharm',
+    isCute: false,
+    friends: [],
+  },
+];
